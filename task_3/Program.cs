cc1c93b950738009f854a68473e8b09128e61a41
Aleksandr Petrov <vigasurt@gmail.com>
2023-12-11 02:45:19 +0300

    Сделал задание номер 3

--- task_3/Program.cs
+++ task_3/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+string? str = "qwrwqq";
+int count = 0;
+for(int i=0;i<str.Length/2;i++){
+    if (str[i]==str[str.Length-i-1]){
+        count++;
+    }
+}
+if (str.Length/2%2==0){
+    if(count==str.Length/2){
+        Console.WriteLine($"Строка {str} является полиндромом");
+    }
+    else{
+        Console.WriteLine($"Строка {str} не является полиндромом");
+    }
+}else{
+        if(count==(str.Length/2)+1){
+        Console.WriteLine($"Строка {str} является полиндромом");
+    }
+    else{
+        Console.WriteLine($"Строка {str} не является полиндромом");
+    }
+}
