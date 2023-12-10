0e350ee1538b6e2563c8509c0d20207505592811
Aleksandr Petrov <vigasurt@gmail.com>
2023-12-11 02:17:17 +0300

    Сделал задание номер 2

--- task_2/Program.cs
+++ task_2/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+Random rand = new Random();
+int N = 10;
+char [] mas = new char [N];
+for (int i=0;i<N;i++){
+    mas[i] = (char)rand.Next(0x0410, 0x44F);
+    Console.Write($"{mas[i]}\t");
+}
+string str = Convert.ToString(mas[0]);
+for(int i=1;i<N;i++){
+    str += Convert.ToString(mas[i]);
+}
+Console.WriteLine($"\n{str.ToLower()}");
